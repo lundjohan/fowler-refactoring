5ed2a5e7fdcc01458e5e77cbf36b62cf4d612c91
Johan Lund <drancisdrake@gmail.com>
2023-04-03 12:49:24 +0200

    removed unused methods

--- printBill.js
+++ printBill.js
@@ -23,31 +23,4 @@ function statement(invoice, plays) {
     //totalAmount & volumeCredits are returned for testing purposes
     return { result, totalAmount, volumeCredits };
 }
-function calcAmount(typeOfPlay, audienceInPlay) {
-    var result = 0;
-    switch (typeOfPlay) {
-        case "tragedy":
-            result = 40000;
-            if (audienceInPlay > 30) {
-                result += 1000 * (audienceInPlay - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (audienceInPlay > 20) {
-                result += 10000 + 500 * (audienceInPlay - 20);
-            }
-            result += 300 * audienceInPlay;
-            break;
-        default:
-            throw new Error(`unknown type: $(play.type)`);
-    }
-    return result;
-}
-function calcVolumeCredits(typeOfPlay, audienceInPlay) {
-    volumeCredits += Math.max(audienceInPlay - 30, 0);
-
-    //add extra credits for every ten comedy attendees
-    if ("comedy" === typeOfPlay) volumeCredits += Math.floor(audienceInPlay / 10);
-}
 exports.statement = statement;
