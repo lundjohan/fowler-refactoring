671b7f4b63db6c04fae14b71677e14f678baed1c
Johan Lund <drancisdrake@gmail.com>
2023-04-20 11:12:13 +0200

    removed unused variable

--- test/statement_test.js
+++ test/statement_test.js
@@ -5,7 +5,6 @@ const statementData = require('../statementData').statementData;
 var assert = require('assert');
 
 let result = statement.statement(invoices,playsJSON);
-let result2 = statement.statement(invoices,playsJSON);
 describe('statement', function () {
     it('result should contain string Amount owed is $1,730.00', function () {
         assert.equal(result.includes("Amount owed is $1,730.00"), true);
