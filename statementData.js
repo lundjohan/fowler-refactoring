581ec7f8f7cd8c76541f70c271be21730c5a6080
Johan Lund <drancisdrake@gmail.com>
2023-04-23 22:33:26 +0200

    enrichPerformances -> enrichPerformance

--- statementData.js
+++ statementData.js
@@ -2,11 +2,12 @@ var retrieveCalculator = require('./calculator').retrievePlay;
 function statementData(anInvoice, playsJSON) {
     let result = {};
     result.customer = anInvoice.customer;
-    result.performances = enrichPerformances(anInvoice.performances);
+    //result.performances = enrichPerformances(anInvoice.performances);
+    result.performances = anInvoice.performances.map(enrichPerformance);
     result.totalAmount = result.performances.reduce((tot, e) => tot + e.amount, 0);
     result.volumeCredits = result.performances.reduce((tot, e) => tot + e.volumeCredits, 0);
     return result;
-    function enrichPerformances(performances) {
+    /*function enrichPerformances(performances) {
         let result = [];
         for (let performance of performances) {
             let play = playsJSON[performance.playID];
@@ -16,6 +17,13 @@ function statementData(anInvoice, playsJSON) {
             perf.volumeCredits = retrieveCalculator(play, performance.audience).volumeCredits();
             result.push(perf);
         }
+        return result;
+    }*/
+    function enrichPerformance(aPerformance) {
+        let result = Object.assign({}, aPerformance);
+        result.play = playsJSON[aPerformance.playID];
+        result.amount = retrieveCalculator(result.play, result.audience).amount();
+        result.volumeCredits = retrieveCalculator(result.play, result.audience).volumeCredits();
         return result;
     }
 }
