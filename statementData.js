f837a736cfb03db2619c4890b95d7190c28f8631
Johan Lund <drancisdrake@gmail.com>
2023-04-23 22:35:47 +0200

    removed unused

--- statementData.js
+++ statementData.js
@@ -2,23 +2,10 @@ var retrieveCalculator = require('./calculator').retrievePlay;
 function statementData(anInvoice, playsJSON) {
     let result = {};
     result.customer = anInvoice.customer;
-    //result.performances = enrichPerformances(anInvoice.performances);
     result.performances = anInvoice.performances.map(enrichPerformance);
     result.totalAmount = result.performances.reduce((tot, e) => tot + e.amount, 0);
     result.volumeCredits = result.performances.reduce((tot, e) => tot + e.volumeCredits, 0);
     return result;
-    /*function enrichPerformances(performances) {
-        let result = [];
-        for (let performance of performances) {
-            let play = playsJSON[performance.playID];
-            let perf = Object.assign({}, performance);
-            perf.play = playsJSON[performance.playID];
-            perf.amount = retrieveCalculator(play, performance.audience).amount();
-            perf.volumeCredits = retrieveCalculator(play, performance.audience).volumeCredits();
-            result.push(perf);
-        }
-        return result;
-    }*/
     function enrichPerformance(aPerformance) {
         let result = Object.assign({}, aPerformance);
         result.play = playsJSON[aPerformance.playID];
